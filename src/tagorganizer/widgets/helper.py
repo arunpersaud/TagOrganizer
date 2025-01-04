6729938fdff3daa1812abba74d7cb30b75781f67
Arun Persaud <apersaud@lbl.gov>
2025-01-03 17:08:36 -0800

    fix orientation of images during loading based on EXIF data

--- src/tagorganizer/widgets/helper.py
+++ src/tagorganizer/widgets/helper.py
@@ -19,23 +19,63 @@ along with TagOrganizer. If not, see <https://www.gnu.org/licenses/>.
 """
 
 from functools import lru_cache
+from pathlib import Path
 
 from qtpy.QtWidgets import QCompleter
-from qtpy.QtGui import QPixmap
+from qtpy.QtGui import QPixmap, QTransform
 from qtpy.QtCore import Qt
+
+import exifread as exif
 
 
 @lru_cache(1_000)
 def load_pixmap(file):
     pixmap = QPixmap(file)
     pixmap = pixmap.scaledToWidth(150, Qt.SmoothTransformation)
+    orientation = get_orientation(file)
+    pixmap = rotate_pixmap(pixmap, orientation)
+
     return pixmap
 
 
 @lru_cache(100)
 def load_full_pixmap(file):
     pixmap = QPixmap(file)
+    orientation = get_orientation(file)
+    pixmap = rotate_pixmap(pixmap, orientation)
+
     return pixmap
+
+
+@lru_cache(1_000)
+def load_exif(file):
+    file = Path(file)
+    if not file.is_file():
+        return {}
+    with file.open("rb") as f:
+        tags = exif.process_file(f)
+        return tags
+
+
+def get_orientation(file):
+    tags = load_exif(file)
+    orientation_tag = "Image Orientation"
+    if orientation_tag in tags:
+        orientation = tags[orientation_tag].values[0]
+    else:
+        orientation = 1  # Default to normal orientation if tag is not found
+    return orientation
+
+
+def rotate_pixmap(pixmap, orientation):
+    transform = QTransform()
+    if orientation == 3:
+        transform.rotate(180)
+    elif orientation == 6:
+        transform.rotate(90)
+    elif orientation == 8:
+        transform.rotate(-90)
+    return pixmap.transformed(transform, Qt.SmoothTransformation)
 
 
 class CommaCompleter(QCompleter):
